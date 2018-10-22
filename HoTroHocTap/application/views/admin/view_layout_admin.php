2678cb7aa9133e0b3c04a53051d8a7caa43318bf
daodhoa <duyhoa.dao1207@gmail.com>
2018-10-22 16:01:30 +0700

    update csdl

--- HoTroHocTap/application/views/admin/view_layout_admin.php
+++ HoTroHocTap/application/views/admin/view_layout_admin.php
@@ -175,18 +175,18 @@
 
 <<<<<<< HEAD
 =======
-    <!-- Core Scripts - Include with every page -->
+<!--
     <script src="<?php echo base_url() ?>bootstrap/assets/plugins/jquery-1.10.2.js"></script>
     <script src="<?php echo base_url() ?>bootstrap/assets/plugins/bootstrap/bootstrap.min.js"></script>
     <script src="<?php echo base_url() ?>bootstrap/assets/plugins/metisMenu/jquery.metisMenu.js"></script>
     <script src="<?php echo base_url() ?>bootstrap/assets/plugins/pace/pace.js"></script>
     <script src="<?php echo base_url() ?>bootstrap/assets/scripts/siminta.js"></script>
-    <!-- Page-Level Plugin Scripts-->
+    
     <script src="<?php echo base_url() ?>bootstrap/assets/plugins/morris/raphael-2.1.0.min.js"></script>
     <script src="<?php echo base_url() ?>bootstrap/assets/plugins/morris/morris.js"></script>
     <script src="<?php echo base_url() ?>bootstrap/assets/scripts/dashboard-demo.js"></script>
     <script src="<?php echo base_url() ?>bootstrap/js/jquery.min.js"></script>
-
+-->
 >>>>>>> ngohoa
 </body>
 
